29cc1ddd0965121deb704957010dcbfc6fbe2900
백상욱 <oogab@naver.com>
2021-10-22 20:08:57 +0900

    초기 커밋.

---
+++ Algorithm_python/boj_2667.py
@@ -0,0 +1,10 @@
+
+from collections import deque, Counter
+from functools import reduce
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+
+n = int(input())
+a = [list(map(int,list(input()))) for _ in range(n)]
+
+print(a)
